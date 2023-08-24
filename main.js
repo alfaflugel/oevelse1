11b0cec8a2b6eb59798a79389b5f13a10019facf
Frederikke <frfl0001@stud.kea.dk>
2023-08-24 10:31:04 +0200

    oevelse2

---
+++ main.js
@@ -0,0 +1,14 @@
+const header = document.querySelector("header");
+console.log(header);
+
+const tekst = document.querySelector("p");
+console.log(tekst);
+
+const overskrift3 = document.querySelector("h3");
+console.log("overskrift3");
+
+const footer = document.querySelector("footer");
+console.log("footer");
+
+const overskrift2 = document.querySelector("h2");
+console.log("overskrift2");
